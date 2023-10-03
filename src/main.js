23965036f5c74c0f8538afc2916f671a5609ff7d
andrewjruss7 <andrewjruss7@gmail.com>
2023-10-03 14:25:39 -0500

    ok

--- src/main.js
+++ src/main.js
@@ -1,4 +1,20 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+import BootstrapVue3 from "bootstrap-vue-3";
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+const app = createApp(App);
+
+app.mount("#app")
+app.use(BootstrapVue3)
+
+import VueAxios from 'vue-axios'
+import Axios from 'axios'
+
+app.config.productionTip = false
+app.use(VueAxios, Axios)
+
+
